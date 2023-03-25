eb30a0dfe6fb89c263827574d424868c2ceefe16
Anton Che <anton.chebykin@gmail.com>
2023-03-25 21:50:16 +1000

    добавлен запрос на ввод элементов массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array1 = new string[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    string result = Console.ReadLine();
+    array1[i] = result;
+}
