6369e9619d50982d94c15e53e0194651e42d541a
Anton Che <anton.chebykin@gmail.com>
2023-03-25 21:58:05 +1000

    добавлен цикл для 2 массива, вывод массивов на экран

--- Task/Program.cs
+++ Task/Program.cs
@@ -24,3 +24,17 @@ for (int i = 0; i < array1.Length; i++)
         count++;
     }
 }
+Console.WriteLine();
+Console.Write("Полученный массив: [");
+string[] array2 = new string[count];
+int j = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= maxChar)
+    {
+        array2[j] = array1[i];
+        Console.Write($"{array2[j]}, ");
+        j++;
+    }
+}
+Console.Write("]");
